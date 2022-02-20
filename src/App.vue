ca83c7ca048f4d391a9831d1ea85b5d59246b5de
Tony Bisegna <tony.bisegna@amadeus.com>
2022-02-20 08:15:18 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Pizza Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/logo.svg"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h2 class="">Pizza BiS</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+            
+          <v-btn
+            class="ma-1"
+            small
+            fab
+            @click="$refs.main.openCart()"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-cart</v-icon>
+          </v-btn>
+        </template>
+        <span>Ouvrir mon panier</span>
+      </v-tooltip>
+      <SwitchDarkMode />
+    </v-app-bar>
+
+    <v-main>
+      <RecipesList ref="main" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RecipesList from "./components/RecipesList";
+import SwitchDarkMode from "./components/SwitchDarkMode";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    RecipesList,
+    SwitchDarkMode,
+  },
+
+  data: () => ({}),
+};
+</script>
